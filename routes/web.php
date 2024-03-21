f67b06f1248ddb4462bd0d3fd00bab82a3bf5e69
SyahiraIsnaeni <syahiraisnaeni15@gmail.com>
2024-03-21 13:40:10 +0700

    bikin login view

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/contoh', function () {
-    return view('contoh');
+Route::get('/login', function () {
+    return view('front.login');
 });
